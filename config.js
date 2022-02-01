f66760f47807714b68e6b1b31defef9649d53159
Nathan James <nathanjms@outlook.com>
2022-02-01 19:32:31 +0000

    Update logic to work with Heroku PostgreSQL Add-on DB

--- config.js
+++ config.js
@@ -4,13 +4,11 @@ require("dotenv").config();
 
 const isProduction = process.env.NODE_ENV === "production";
 
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-  host: process?.env?.DB_HOST ? process?.env?.DB_HOST : 'nj-db',
-  port: process?.env?.DB_PORT ? process?.env?.DB_PORT : '5432',
-  user: process?.env?.DB_USER ? process?.env?.DB_USER : 'nj',
-  password: process?.env?.DB_PASSWORD ? process?.env?.DB_PASSWORD : 'dev',
-  database: process?.env?.DB_DATABASE ? process?.env?.DB_DATABASE : 'nathanjms_api',
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
-})
+});
 
 module.exports = { pool };
