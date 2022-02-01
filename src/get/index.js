1ea5a2859b542e8ea4acc6ea7eb646e50bd86d65
Nathan James <nathanjms@outlook.com>
2022-02-01 18:57:01 +0000

    Wording in greeting update to specify

--- src/get/index.js
+++ src/get/index.js
@@ -1,7 +1,7 @@
 const index = (request, response, next) => {
   response
     .status(200)
-    .json({ greeting: "Welcome to my API!", author: "Nathan James" });
+    .json({ greeting: "Welcome to my Node API!", author: "Nathan James" });
 };
 
 export { index };
