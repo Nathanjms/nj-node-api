6b51b108550908dfd0d7dbbe905337ead9043223
Nathan James <nathanjms@outlook.com>
2022-02-01 19:07:10 +0000

    Add ternaries if there is no .env file set

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process?.env?.DB_HOST ? process?.env?.DB_HOST : 'nj-db',
+  port: process?.env?.DB_PORT ? process?.env?.DB_PORT : '5432',
+  user: process?.env?.DB_USER ? process?.env?.DB_USER : 'nj',
+  password: process?.env?.DB_PASSWORD ? process?.env?.DB_PASSWORD : 'dev',
+  database: process?.env?.DB_DATABASE ? process?.env?.DB_DATABASE : 'nathanjms_api',
   ssl: isProduction ? { rejectUnauthorized: false } : false,
 })
 
