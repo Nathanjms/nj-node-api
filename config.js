0055d7354545f4d38752eadac4197973049198a0
Nathan James <nathanjms@outlook.com>
2022-02-01 18:56:28 +0000

    Switch variables to be env

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-  host: 'nj-node-db',
-  port: 5432,
-  user: 'nj',
-  password: 'dev',
-  database: 'nathanjms_api',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
 })
 
