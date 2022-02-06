a5dee3c0d8637e64f05562757a035c53d88755c8
Nathan James <nathanjms@outlook.com>
2022-02-06 20:29:39 +0000

    Models

---
+++ src/models/index.js
@@ -0,0 +1,29 @@
+const { connectionString } = require("../config/db.config.js");
+const userModel = require("../models/user.model.js");
+const roleModel = require("../models/role.model.js");
+
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(connectionString);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = userModel(sequelize, Sequelize);
+db.role = roleModel(sequelize, Sequelize);
+
+db.role.belongsToMany(db.user, {
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId",
+});
+db.user.belongsToMany(db.role, {
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId",
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
