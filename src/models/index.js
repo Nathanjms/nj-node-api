3f14a2f893cd9c7fe4be6527ad0aa844fb0a719d
Nathan James <nathanjms@outlook.com>
2022-02-06 20:53:22 +0000

    testConnection

--- src/models/index.js
+++ src/models/index.js
@@ -5,25 +5,36 @@ const { roleModel } = require("../models/role.model.js");
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(connectionString);
 
+const testConnection = async (sequelize) => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+} catch (error) {
+    console.error("Unable to connect to the database:", error);
+    console.log(connectionString);
+  }
+};
+
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+testConnection(db.Sequelize);
 
-db.user = userModel(sequelize, Sequelize);
-db.role = roleModel(sequelize, Sequelize);
+// db.user = userModel(sequelize, Sequelize);
+// db.role = roleModel(sequelize, Sequelize);
 
-db.role.belongsToMany(db.user, {
-  through: "user_roles",
-  foreignKey: "roleId",
-  otherKey: "userId",
-});
-db.user.belongsToMany(db.role, {
-  through: "user_roles",
-  foreignKey: "userId",
-  otherKey: "roleId",
-});
+// db.role.belongsToMany(db.user, {
+//   through: "user_roles",
+//   foreignKey: "roleId",
+//   otherKey: "userId",
+// });
+// db.user.belongsToMany(db.role, {
+//   through: "user_roles",
+//   foreignKey: "userId",
+//   otherKey: "roleId",
+// });
 
-db.ROLES = ["user", "admin", "moderator"];
+// db.ROLES = ["user", "admin", "moderator"];
 
 module.exports = db;
