b6f8f80bb9bcf74fce2e48ba94c6ebcec967562a
Nathan James <nathanjms@outlook.com>
2022-02-06 20:59:10 +0000

    Take out db seeding in server.js (moved to seeder)

--- src/server.js
+++ src/server.js
@@ -13,32 +13,6 @@ app.use(express.json());
 // Parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-/* DB & Model */
-
-const db = require("./models");
-const Role = db.role;
-db.sequelize.sync().then(() => {
-  console.log('Drop and Resync Db');
-  initial();
-});
-
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
-  });
- 
-  Role.create({
-    id: 2,
-    name: "moderator"
-  });
- 
-  Role.create({
-    id: 3,
-    name: "admin"
-  });
-}
-
 /* Routes */
 
 // Basic route
@@ -51,7 +25,6 @@ app.route("/").get((request, response) => {
 // Routes from route configs
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
-
 
 // Start server
 let port = process?.env?.PORT ? process?.env?.PORT : 3002;
