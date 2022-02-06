90aefd96605b74ad781a070b54173d3b020d8e27
Nathan James <nathanjms@outlook.com>
2022-02-06 20:53:04 +0000

    Fix db.config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
 
 const connectionString = 
-  `postgresql://
-    ${process.env.DB_USER}:
-    ${process.env.DB_PASSWORD}@
-    ${process.env.DB_HOST}:
-    ${process.env.DB_PORT}/
-    ${process.env.DB_DATABASE}`
-  ;
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 module.exports = { connectionString };
