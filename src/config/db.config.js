0ee53633f914e89fea0f9af56c5333e9b2f1f8c5
Nathan James <nathanjms@outlook.com>
2022-02-06 20:58:34 +0000

    add isProduction const (might move this location later)

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const connectionString = 
   `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-module.exports = { connectionString };
+module.exports = { connectionString, isProduction };
