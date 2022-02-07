91146996455f0a9ecfe4dcf77d5d239a868249d7
Nathan James <nathanjms@outlook.com>
2022-02-07 22:43:14 +0000

    Create user model

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+const { pg } = require("../config/db.config");
+
+exports.insertUser = async (name, email, password) => {
+  await pg("users")
+    .insert({
+      name: name,
+      email: email,
+      password: password,
+    })
+    .then(() => {
+      return Promise.resolve();
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
+
+exports.getUserFromEmail = async (email) => {
+  return await pg("users").first().from("users").where("email", email);
+};
