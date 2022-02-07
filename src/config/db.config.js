b1ccb02938543a50d5c497e2686583041a544a1b
Nathan James <nathanjms@outlook.com>
2022-02-07 22:43:18 +0000

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -6,6 +6,10 @@ const { connectionString } = require("../../knexfile");
 const pg = require("knex")({
   client: "pg",
   connection: connectionString,
+  pool: {
+    min: 2,
+    max: 10,
+  },
 });
 
 module.exports = { pg, isProduction };
