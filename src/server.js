442986ab669911070d52b56d5d28b054a1dc131b
Nathan James <nathanjms@outlook.com>
2022-02-08 22:01:08 +0000

    Test endpoint for testing JWT

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express from "express";
+var jwt = require("jsonwebtoken");
+const config = require("./config/auth.config");
 
 const app = express();
 
@@ -22,8 +24,12 @@ app.route("/").get((request, response) => {
     .json({ greeting: "Welcome to my Node API!", author: "Nathan James" });
 });
 
+app.get("/api/test", (req, res, next) => {
+  verifyToken(req, res, next);
+});
+
 // // Routes from route configs
-require('./routes/auth.routes')(app);
+require("./routes/auth.routes")(app);
 // require('./routes/user.routes')(app);
 
 // Start server
@@ -31,3 +37,24 @@ let port = process?.env?.PORT ? process?.env?.PORT : 3002;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  let token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(403).send({
+      message: "No token provided!",
+    });
+  }
+
+  jwt.verify(token, "njdev", (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!",
+      });
+    }
+    req.userId = decoded.id;
+    return res.status(200).send({ success: true, userId: req.userId });
+  });
+};
