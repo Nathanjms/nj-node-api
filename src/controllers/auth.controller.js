46851241986308e9f3030bd67e7c6ae119b86fb0
Nathan James <nathanjms@outlook.com>
2022-02-08 21:50:28 +0000

    Clean up await functions with try catch block

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,70 +1,47 @@
-const User = require("../models/User");
-const {
-  verifyNoValidationErrors,
-} = require("../middleware/verifyNoValidationErrors");
-
+const User = require("../models/user.model");
+const config = require("../config/auth.config");
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
 exports.signup = async (req, res, next) => {
-  console.log(bcrypt.hashSync(req.body.password, 8));
-  await User.insertUser(
-    req.body.name,
-    req.body.email,
-    bcrypt.hashSync(req.body.password, 8)
-  )
-    .then(() => {
-      res.send({ message: "User was registered successfully!" });
-    })
-    .catch((err) => {
-      next(err);
-    });
+  try {
+    let existingUser = await User.getUserFromEmail(req.body.email);
+    if (existingUser) {
+      return res.status(401).send({ message: "Email Address already exists" });
+    }
+    await User.insertUser(
+      req.body.name,
+      req.body.email,
+      bcrypt.hashSync(req.body.password, 8)
+    );
+    return res.send({ message: "User was registered successfully!" });
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.signin = (req, res, next) => {
-  User.getUserFromEmail(req.body.email)
-    .then((user) => {
-      console.log(user);
-      if (!user || user.length === 0) {
-        return res.status(404).send({ message: "User Not found." });
-      }
-      console.log(req.body.password, user);
-      let passwordIsValid = bcrypt.compareSync(
-        req?.body?.password,
-        user.password
-      );
+exports.signin = async (req, res, next) => {
+  try {
+    let user = await User.getUserFromEmail(req.body.email);
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
+    }
+    let passwordIsValid = bcrypt.compareSync(
+      req?.body?.password,
+      user.password
+    );
 
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!",
-        });
-      }
-
-      res.status(200).send(user);
-    })
-    .catch((err) => {
-      next(err);
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!",
+      });
+    }
+    var token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400, // 24 hours
     });
-  //     var token = jwt.sign({ id: user.id }, config.secret, {
-  //       expiresIn: 86400, // 24 hours
-  //     });
-
-  //     var authorities = [];
-  //     user.getRoles().then((roles) => {
-  //       for (let i = 0; i < roles.length; i++) {
-  //         authorities.push("ROLE_" + roles[i].name.toUpperCase());
-  //       }
-  //       res.status(200).send({
-  //         id: user.id,
-  //         username: user.username,
-  //         email: user.email,
-  //         roles: authorities,
-  //         accessToken: token,
-  //       });
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).send({ message: err.message });
-  //   });
+    return res.status(200).send({ token: token, name: user.name });
+  } catch (error) {
+    next(error);
+  }
 };
