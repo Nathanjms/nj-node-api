81e8453109f35b7db9b4b12f6c2e38486e1a360d
Nathan James <nathanjms@outlook.com>
2022-02-10 21:33:16 +0000

    Update cors and remove unused method

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "*.nathanjms.co.uk",
 };
 
 app.use(cors(corsOptions));
@@ -22,16 +22,12 @@ app.route("/").get((request, response) => {
     .json({ greeting: "Welcome to my Node API!", author: "Nathan James" });
 });
 
-app.get("/api/test", (req, res, next) => {
-  verifyToken(req, res, next);
-});
-
 // Routes from route configs
 require("./routes/auth.routes")(app);
 require('./routes/user.routes')(app);
 
 // Start server
-let port = process?.env?.PORT ? process?.env?.PORT : 3002;
+let port = process?.env?.PORT ? process.env.PORT : 3002;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
