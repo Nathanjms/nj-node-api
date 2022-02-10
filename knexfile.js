e75bad134b2607e6fc072d28c9a0982cfbca2e06
Nathan James <nathanjms@outlook.com>
2022-02-10 21:00:52 +0000

    ssl db connection

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = isProduction 
+const connectionString = isProduction
   ? process.env.DATABASE_URL
   : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
@@ -12,7 +12,10 @@ const connectionString = isProduction
  */
 module.exports = {
   client: "postgresql",
-  connection: connectionString,
+  connection: {
+    connectionString: connectionString,
+    ssl: { rejectUnauthorized: isProduction },
+  },
   pool: {
     min: 2,
     max: 10,
