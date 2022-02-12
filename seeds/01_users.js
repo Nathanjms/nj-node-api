99190fa283e58129cf87ccc58e6483ab144c0f44
Nathan James <nathanjms@outlook.com>
2022-02-12 10:38:51 +0000

    Seeds

---
+++ seeds/01_users.js
@@ -0,0 +1,24 @@
+const bcrypt = require("bcryptjs");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .del()
+    .then(function () {
+      // Build array of entries
+      let insertArray = [];
+      for (let i = 1; i <= 10; i++) {
+        insertArray.push({
+          id: i,
+          email: `nj${i}@test.com`,
+          password: bcrypt.hashSync("123456", 8),
+          name: `NJ${i}`,
+        });
+      }
+      return knex("users").insert(insertArray);
+    });
+};
---
+++ seeds/02_movies.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('movies').del()
+    .then(function () {
+      // Build array of entries
+      let insertArray = [];
+      for (let i = 1; i <= 10; i++) {
+        insertArray.push({
+          id: i,
+          title: `Movie${i}`,
+          tmdb_id: `1234567`,
+          poster_path: `/test`,
+          user_id: Math.floor((Math.random() * 10) + 1) // Random user between ID 1 and 10
+        });
+      }
+      // Inserts seed entries
+      return knex('movies').insert(insertArray);
+    });
+};
