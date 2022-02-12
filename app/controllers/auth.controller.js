6ef5ad3588365b6c52f3cf145d763d1e47d679ca
Nathan James <nathanjms@outlook.com>
2022-02-12 14:53:36 +0000

    Signup controller

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const config = require("../config/auth.config");
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
-exports.signup = async (req, res, next) => {
+exports.register = async (req, res, next) => {
   try {
     let existingUser = await User.getUserFromEmail(req.body.email, true);
     if (existingUser) {
@@ -20,23 +20,17 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-exports.signin = async (req, res, next) => {
+exports.signIn = async (req, res, next) => {
   try {
     let user = await User.getUserFromEmail(req.body.email);
-    if (!user) {
-      return res.status(404).send({ message: "User Not found." });
-    }
-    let passwordIsValid = bcrypt.compareSync(
-      req?.body?.password,
-      user.password
-    );
 
-    if (!passwordIsValid) {
-      return res.status(401).send({
-        accessToken: null,
-        message: "Invalid Password!",
-      });
+    // Dont wan't to do DB calls in middleware (for now at least), so handle user checks relating to database here
+    let isValid = _validUserDetails(user, req.body.password);
+    if (!isValid.valid) {
+      res.status(isValid.code).send({ message: isValid.message });
+      return;
     }
+
     let expiresIn = 86400; // 24 hours
     var token = jwt.sign({ id: user.id }, config.secret, {
       expiresIn: expiresIn,
@@ -45,4 +39,22 @@ exports.signin = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+const _validUserDetails = (user, inputPassword) => {
+  if (!user) {
+    return {
+      valid: false,
+      code: 401,
+      message: "User Not found.",
+    };
+  }
+  if (!bcrypt.compareSync(inputPassword, user.password)) {
+    return {
+      valid: false,
+      code: 401,
+      message: "Invalid Password",
+    };
+  }
+  return { valid: true };
 };
