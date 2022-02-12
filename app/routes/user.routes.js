05e5e8c637a2982cddc917d00b494aada280964f
Nathan James <nathanjms@outlook.com>
2022-02-12 15:27:51 +0000

    controller -> userController

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,9 +1,9 @@
-const controller = require("../controllers/user.controller");
+const userController = require("../controllers/user.controller");
 const authJWT = require("../middleware/authJwt");
 
 module.exports = (app) => {
   // All routes in here will need valid JWT
   app.use(authJWT.verifyToken);
 
-  app.get("/api/user-info", controller.userInfo);
+  app.get("/api/user-info", userController.userInfo);
 };
