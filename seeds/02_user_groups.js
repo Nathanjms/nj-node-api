e7f0e4f4588e1680f902af04acade10bc3d6c33a
Nathan James <nathanjms@outlook.com>
2022-02-12 23:22:35 +0000

    Switch to async await

--- seeds/02_user_groups.js
+++ seeds/02_user_groups.js
@@ -1,30 +1,30 @@
-const bcrypt = require("bcryptjs");
-
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex("user_groups")
-    .del()
-    .then(function () {
-      // Build array of entries
-      let insertArray = [];
-      for (let i = 1; i <= 10; i++) {
-        insertArray.push({
-          id: i,
-          name: `Group ${i}`,
-        });
-      }
-      return knex("user_groups")
-        .insert(insertArray)
-        .then(() => {
-          return knex("users_groups").insert({
-            id: 1,
-            user_id: 1,
-            group_id: 1,
-          });
-        });
+exports.seed = async function (knex) {
+  try {
+    await knex("user_groups").del();
+    let insertArray = buildDataArray();
+    await knex("user_groups").insert(insertArray);
+    await knex("users_groups").del();
+    return await knex("users_groups").insert({
+      id: 1,
+      user_id: 1,
+      group_id: 1,
     });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const buildDataArray = () => {
+  let insertArray = [];
+  for (let i = 1; i <= 10; i++) {
+    insertArray.push({
+      id: i,
+      name: `Group ${i}`,
+    });
+  }
+  return insertArray;
 };
