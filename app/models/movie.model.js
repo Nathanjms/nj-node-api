266b7ad953e42ea9f031230f2550c1b5bb62bd6b
Nathan James <nathanjms@outlook.com>
2022-02-13 15:27:35 +0000

    add getByGroupId method & select column to model

--- app/models/movie.model.js
+++ app/models/movie.model.js
@@ -9,6 +9,7 @@ const columnWhitelist = [
   "poster_path",
   "rating",
   "seen",
+  "group_id",
 ];
 
 const selectColumns = columnWhitelist.map((element) => {
@@ -17,6 +18,16 @@ const selectColumns = columnWhitelist.map((element) => {
 
 exports.getMoviesByUserId = async (userId, includeDeleted = false) => {
   let movies = pg(table).select(selectColumns).where("user_id", userId);
+
+  if (!includeDeleted) {
+    movies = movies.where("deleted_at", null);
+  }
+
+  return await movies;
+};
+
+exports.getMoviesByGroupId = async (groupId, includeDeleted = false) => {
+  let movies = pg(table).select(selectColumns).where("group_id", groupId);
 
   if (!includeDeleted) {
     movies = movies.where("deleted_at", null);
