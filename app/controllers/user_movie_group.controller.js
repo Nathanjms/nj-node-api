0f6b4c63ab39728badf1cda9d64c7a5259f56816
Nathan James <nathanjms@outlook.com>
2022-02-13 18:21:23 +0000

    Movie Group Controller

--- app/controllers/user_movie_group.controller.js
+++ app/controllers/user_movie_group.controller.js
@@ -2,8 +2,10 @@ const UserMovieGroup = require("../models/user_movie_group.model");
 
 exports.index = async (req, res, next) => {
   try {
-    let userMovieGroups = await UserMovieGroup.getUserMovieGroupsByUserId(req.userId);
-    return res.status(200).send({ userMovieGroups: userMovieGroups });
+    let userMovieGroups = await UserMovieGroup.getUserMovieGroupsByUserId(
+      req.userId
+    );
+    return res.send({ userMovieGroups: userMovieGroups });
   } catch (error) {
     next(error);
   }
@@ -12,6 +14,7 @@ exports.index = async (req, res, next) => {
 exports.store = async (req, res, next) => {
   try {
     // Create group
+    return res.send({ message: "store" });
   } catch (error) {
     next(error);
   }
@@ -20,6 +23,7 @@ exports.store = async (req, res, next) => {
 exports.leaveGroup = async (req, res, next) => {
   try {
     // Delete user's entry in the group
+    return res.send({ message: "leaveGroup" });
   } catch (error) {
     next(error);
   }
@@ -28,8 +32,8 @@ exports.leaveGroup = async (req, res, next) => {
 exports.delete = async (req, res, next) => {
   try {
     // Unsure if this will be added, or just allow leaving above
+    return res.send({ message: "delete" });
   } catch (error) {
     next(error);
   }
 };
-
