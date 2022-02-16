0c9ead2df8339287821aea0e6ed6fa39b2d6dbd5
Nathan James <nathanjms@outlook.com>
2022-02-16 20:11:44 +0000

    Migrations

--- migrations/20220216192507_add_password_column_to_user_movie_groups.js
+++ migrations/20220216192507_add_password_column_to_user_movie_groups.js
@@ -2,18 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.alterTable("user_groups", (table) => {
-        table.text("password").notNullable();
-      })
+exports.up = function (knex) {
+  return knex.schema.alterTable("user_groups", (table) => {
+    table.text("password").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.alterTable("user_groups", (table) => {
-      table.dropColumn("password");
-  })
+    table.dropColumn("password");
+  });
 };
