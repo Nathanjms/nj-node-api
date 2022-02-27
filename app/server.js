9ca587991454ebc88a6220a408c2b87506bc0062
Nathan James <nathanjms@outlook.com>
2022-02-27 10:50:14 +0000

    Try changing cors?

--- app/server.js
+++ app/server.js
@@ -3,19 +3,11 @@ import express from "express";
 
 const app = express();
 
-const allowedOrigins = ["https://*.nathanjms.co.uk", "http://localhost:3000"];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://*.nathanjms.co.uk"],
+  })
+);
 // Parse requests of content-type - application/json
 app.use(express.json());
 // Parse requests of content-type - application/x-www-form-urlencoded
