a54e0915d4c1a38053e7224d5eff58d8639f43ea
Nathan James <nathanjms@outlook.com>
2022-03-12 16:30:01 +0000

    Real json example for seeder

--- seeds/03_movies.js
+++ seeds/03_movies.js
@@ -8,18 +8,17 @@ exports.seed = function (knex) {
     .del()
     .then(function () {
       // Build array of entries
-      let insertArray = [];
-      for (let i = 1; i <= 100; i++) {
-        let userId = Math.floor(Math.random() * 10 + 1);
+      let insertArray = require("../app/config/movies.example.json");
+      for (let i = 11; i <= 20; i++) {
+        let seen = i % 2; // 50% chance of marking as seen
         insertArray.push({
-          title: `Movie${i}`,
-          tmdb_id: `1234567`,
-          poster_path: `/test`,
-          user_id: i <= 50 ? userId : null, // First half assigned to user
-          group_id: i <= 50 ? null : 1, // Second half assigned to group
-          seen: i % 2,
-          created_by: i <= 50 ? userId : 1,
-          rating: Math.floor(Math.random() * 5 + 1),
+          title: `Movie ${i}`,
+          tmdb_id: ``,
+          poster_path: ``,
+          group_id: 1,
+          seen: seen,
+          created_by: 1,
+          rating: seen ? Math.floor(Math.random() * 5 + 1) : null,
         });
       }
 
