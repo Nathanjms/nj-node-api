346457af3e3b0ebf113af633b792fed2ce31e7be
Nathan James <nathanjms@outlook.com>
2022-03-13 10:52:29 +0000

    Add table backdrop_path

---
+++ migrations/20220313103807_add_backdrop_path_to_movies_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("movies", (table) => {
+    table.string("backdrop_path", 255);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("movies", (table) => {
+    table.dropColumn("backdrop_path");
+  });
+};
